e75e3cab86da1cfa75fcc925967618b7778092bb
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-27 02:02:09 +0300

    Update App.jsx

--- src/i18n/src/i18n/src/App.jsx
+++ src/i18n/src/i18n/src/App.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
-import HowItWorks from "./pages/HowItWorks";
 import Methodology from "./pages/Methodology";
+import HowItWorks from "./pages/HowItWorks";
 import Assessment from "./pages/Assessment";
 import Result from "./pages/Result";
 import Privacy from "./pages/Privacy";
 import Disclaimer from "./pages/Disclaimer";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   const { i18n } = useTranslation();
   const [dir, setDir] = useState("rtl");
 
@@ -21,15 +23,15 @@ function App() {
   }, [i18n.language]);
 
   return (
-    <div dir={dir} className="min-h-screen bg-[#0b1020] text-white">
-      <BrowserRouter>
+    <div dir={dir} className="min-h-screen bg-gray-100">
+      <HashRouter>
         <Header />
         <div className="p-4">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/how" element={<HowItWorks />} />
             <Route path="/method" element={<Methodology />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
             <Route path="/assess" element={<Assessment />} />
             <Route path="/result" element={<Result />} />
             <Route path="/privacy" element={<Privacy />} />
@@ -37,9 +39,7 @@ function App() {
           </Routes>
         </div>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
-
-export default App;
