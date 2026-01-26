6dc5655cbbae70182d4c672a9b4dd2e501e54965
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-27 00:12:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const e = React.createElement;
 
 function App() {
   return e("div", { style: { fontFamily: "Arial, sans-serif", padding: "20px" } }, [
-    e("h1", { style: { color: "#0f172a" } }, "أداة تقييم السلامة الإنشائية"),
-    e("p", null, "الآن React يشتغل بدون Build.")
+    e("h1", { style: { color: "#0f172a" } }, "React Works!"),
+    e("p", null, "إذا ظهر هذا النص، React شغال.")
   ]);
 }
 
