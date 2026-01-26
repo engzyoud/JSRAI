a8170b2e4a714e16a7c6a7b1bb13162244aa036d
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-26 23:03:24 +0300

    Delete src directory

--- src/src/src/src/data/src/src/components/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer({ t }) {
-  return (
-    <div className="card container" style={{ textAlign: "center" }}>
-      {t.footer}
-    </div>
-  );
-}
--- src/src/src/src/data/src/src/components/src/components/src/pages/src/pages/src/pages/src/components/ProgressBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ProgressBar({ value }) {
-  return (
-    <div className="progress" style={{ marginTop: 12 }}>
-      <div style={{ width: `${value}%` }} />
-    </div>
-  );
-}
