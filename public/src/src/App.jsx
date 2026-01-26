96897d3e084b96a417deb8abbdde4f81d12ab0f2
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-27 01:09:29 +0300

    Create App.jsx

---
+++ public/src/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Intro from "./components/Intro";
+import HowItWorks from "./components/HowItWorks";
+import Assessment from "./components/Assessment";
+import Footer from "./components/Footer";
+
+export default function App(){
+  return (
+    <>
+      <Header/>
+      <Intro/>
+      <HowItWorks/>
+      <Assessment/>
+      <Footer/>
+    </>
+  );
+}
