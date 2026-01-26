312aadde9e18d79f078480748a79055babc27f62
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-27 01:10:30 +0300

    Create Header.jsx

---
+++ public/src/src/src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+  return(
+    <section style={{textAlign:"center"}}>
+      <div className="badge">بسم الله الرحمن الرحيم</div>
+      <h1>أداة التقييم الأولي للسلامة الإنشائية</h1>
+      <p>أداة استرشادية مهنية مبنية على مبادئ هندسية – لا تُغني عن الفحص الموقعي</p>
+    </section>
+  );
+}
