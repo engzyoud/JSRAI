5cd11b50494e03ee329cfb631781e1ab4404d562
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-29 10:55:38 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { LangProvider } from './context/LangContext'
-import Layout from './pages/Layout'
-import Home from './pages/Home'
-import HowItWorks from './pages/HowItWorks'
-import Methodology from './pages/Methodology'
-import Assessment from './pages/Assessment'
-import Result from './pages/Result'
-import About from './pages/About'
-import Privacy from './pages/Privacy'
-import Disclaimer from './pages/Disclaimer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { LangProvider } from './LangContext'
+import Navbar from './Navbar'
+import Home from './Home'
+import Footer from './Footer'
+import './global.css'
 
-export default function App() {
+function App() {
   return (
     <LangProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="how" element={<HowItWorks />} />
-          <Route path="method" element={<Methodology />} />
-          <Route path="assessment" element={<Assessment />} />
-          <Route path="result" element={<Result />} />
-          <Route path="about" element={<About />} />
-          <Route path="privacy" element={<Privacy />} />
-          <Route path="disclaimer" element={<Disclaimer />} />
-        </Route>
-      </Routes>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </LangProvider>
   )
 }
+
+export default App
