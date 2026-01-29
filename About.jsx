74dfd9576948b18318039b77db7a387c3d4fbaef
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-29 22:57:12 +0300

    Update About.jsx

--- About.jsx
+++ About.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import { useLang } from '../context/LangContext'
+import React from "react";
+import { useLang } from "../context/LangContext";
 
 export default function About() {
-  const { t } = useLang()
+  const { t } = useLang();
+
   return (
-    <div>
+    <div className="page">
       <div className="pageHeader">
         <div>
           <h2>{t.about.title}</h2>
           <p>{t.about.p1}</p>
           <p>{t.about.p2}</p>
+          <p>{t.about.p3}</p>
         </div>
       </div>
 
       <div className="card">
         <div className="cardBody">
-          <h3>{t.about.developed}</h3>
-          <p className="smallNote">
-            هذه الأداة مبدئية ومخصصة للتقييم السريع فقط. لا تغني عن فحص مهندس أو اختبار مختبري.
-          </p>
+          <h3>{t.about.scopeTitle}</h3>
+          <p>{t.about.scopeText}</p>
+
+          <h3>{t.about.methodologyTitle}</h3>
+          <p>{t.about.methodologyText}</p>
+
+          <p className="smallNote">{t.about.disclaimer}</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
