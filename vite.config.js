bd1dcc8dd6be5bde8cbd94d9cf6f9f2adc76eeb6
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-29 10:43:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // هاد السطر أهم شي عشان Netlify ما يعطيك شاشة بيضاء
-  base: './', 
+  base: './',
+  build: {
+    outDir: 'dist',
+  }
 })
