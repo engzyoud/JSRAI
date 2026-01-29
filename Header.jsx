c9d6e55cb5c1e858211d416463c7889d3fecbd8e
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-29 23:10:56 +0300

    Delete Header.jsx

--- Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <p className="basmala">بسم الله الرحمن الرحيم</p>
-      <h1>أداة تقييم سلامة المنشأ</h1>
-      <p>JSRAI – Jordan Structural Risk Assessment Index</p>
-    </header>
-  );
-}
