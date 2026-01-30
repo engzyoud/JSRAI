1e45dc1a08a83577cb20e6717c9c1d490e1df761
engzyoud <Eng.zyoud1999@gmail.com>
2026-01-31 01:04:25 +0300

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { LangProvider } from './context/LangContext'
+import './global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LangProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LangProvider>
+  </React.StrictMode>
+)
